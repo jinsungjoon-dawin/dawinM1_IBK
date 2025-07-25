9789fa7605c72c2b7235485a4a5dc8bcd9c63591
jinsungjoon-dawin <sjjin@dawinit.com>
2025-07-25 12:58:41 +0900

    DashBoard.svelte 업데이트

--- src/lib/DashBoard.svelte
+++ src/lib/DashBoard.svelte
@@ -33,15 +33,15 @@
 {#if loading}
 <LoadingOverlay />
 {/if}
-<div class="mx-auto p-3 w-10/12 h-5/6">
+<div class="mx-auto p-2 w-10/12 h-5/6">
 <div class="flex flex-col  max-h-svh">
   {#if rdata}
   <div class=" bg-gray-700 rounded-lg">
     <div class="flex w-full border-b-2 border-gray-500 items-center text-yellow-100">
-        <h1 class="w-3/5 text-2xl tracking-tight p-3">{$t.dash.testTitle} {rdata[0]?.performnm} </h1>
-        <h1 class="w-2/5 text-1xl tracking-tight text-end p-3">{$t.dash.date} {testDate}</h1>
+        <h1 class="w-3/5 text-2xl tracking-tight p-2">{$t.dash.testTitle} {rdata[0]?.performnm} </h1>
+        <h1 class="w-2/5 text-1xl tracking-tight text-end p-2">{$t.dash.date} {testDate}</h1>
     </div>
-      <div class="flex justify-between w-full p-3">
+      <div class="flex justify-between w-full p-2">
         <div class="flex bg-gray-800 px-12 rounded-lg w-1/2 mx-1 justify-center ">
           <div class="size-[320px]" >
             <PieChart page={'S'} selData={rdata[0]} title={rdata[0]?.performnm}></PieChart>
@@ -54,10 +54,10 @@
   </div>
   <div class="bg-gray-700 mt-3 rounded-lg">
     <div class="flex w-full border-b-2 border-gray-500 items-center text-yellow-100 ">
-      <h1 class="w-3/5 text-2xl tracking-tight p-3"> {$t.dash.dataTitle} {rdata[0]?.dataverifynm}</h1>
-      <h1 class="text-1xl w-full text-end tracking-tight p-3">{$t.dash.date}  {rdata[0]?.dataverifydt} </h1>
+      <h1 class="w-3/5 text-2xl tracking-tight p-2"> {$t.dash.dataTitle} {rdata[0]?.dataverifynm}</h1>
+      <h1 class="text-1xl w-full text-end tracking-tight p-2">{$t.dash.date}  {rdata[0]?.dataverifydt} </h1>
     </div>
-      <div class="flex justify-between w-full p-3">
+      <div class="flex justify-between w-full p-2">
         <div class="flex bg-gray-800 px-12 rounded-lg w-1/2 justify-center mx-1">
           <BarChart page={'D'} ></BarChart>
         </div>
